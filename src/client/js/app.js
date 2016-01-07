47da6b630ae49316fdc97548aef1aa56b28f1678
Michael Oakley <Michael.Oakley@nrel.gov>
2016-01-07 11:53:37 -0700

    added mergeParams function + code cleanup

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,46 +4,97 @@
  *  Author: Michael Oakley <moakley.oakley@nrel.gov>
  */
 
- /* jshint unused:false */
+ /* jshint unused:false, debug:true */
 $(document).ready(function() {
     'use strict'
 
-    $('#form-subscribe').submit(function() {
+    /**
+     * Takes an array of objects and merges objects with common names. Values are merged as a comma sep string
+     *
+     * @param  {Array} objArray Array of objects
+     * @return {Array}          Array of objects with unique name properties
+     */
+    var mergeParams = function( objArray ) {
+        var resultsArray = []
+
+        // sort by name
+        objArray.sort( function( a, b ){
+            return a.name - b.name
+        })
+
+        // prime the results array
+        resultsArray.push( objArray.shift() )
+
+        // merge
+        objArray.forEach( function( o, i, a ){
+            var last  = resultsArray[resultsArray.length - 1]
+            if ( o.name === last.name ) {
+                last.value += ',' + o.value
+            } else {
+                resultsArray.push( o )
+            }
+
+        })
+
+        return resultsArray
+    }
+
+    /**
+     * Enable the submit button and show some debugging
+     *
+     * @param  {Object} response JSON returned from AJAX
+     * @return none
+     */
+    var jsonpcallback = function(response) {
+        $('input[type=submit]').removeAttr('disabled')
+        $('#response').removeClass('hidden')
+
+        var text = JSON.stringify(response, null, '&nbsp;')
+
+        $('#response').html('<strong>' + response.message + '</strong><br>' + text)
+    }
+
+    /**
+     *
+     * Send the form data by AJAX
+     *
+     */
+    $('#form-subscribe').submit(function(e) {
+
+        e.preventDefault()
+
         $('input[type=submit]').attr('disabled', 'disabled')
         $('#response').addClass('hidden')
 
         var url = 'https://stage-api.govdelivery.com/api/add_script_subscription'
-        var apikey = 't2iRUf5kNknlGQAO3H_XTbPrGg1sOIo_J1Me_d9vuzKXyiLzvjOakJOjuiJ4b4JinRWzNcik37EtO_zzEflbow'
+          , apikey = 't2iRUf5kNknlGQAO3H_XTbPrGg1sOIo_J1Me_d9vuzKXyiLzvjOakJOjuiJ4b4JinRWzNcik37EtO_zzEflbow'
+          , data
 
         if (!document.getElementById('api') ) {
             $('<input />', {
-                    'id': 'api',
-                    'name': 'k',
-                    'value': apikey,
-                    'type': 'hidden'
+                    'id': 'api'
+                  , 'name': 'k'
+                  , 'value': apikey
+                  , 'type': 'hidden'
                 }).appendTo('#form-subscribe')
         }
 
+        data = $('#form-subscribe').serializeArray()
+
+        data = mergeParams( data )
+
         $.ajax({
-            url: url,
-            data: $('#form-subscribe').serializeArray(),
-            dataType: 'jsonp',
-            jsonp: 'c',
-            jsonpCallback: 'jsonpcallback'
+            url: url
+          , data: data
+          , dataType: 'jsonp'
+          , jsonp: 'c'
+          , jsonpCallback: 'jsonpcallback'
         })
+
         return false
     })
 
 })
 
 
-// Named callback function from the ajax call when jsonpbtn2 clicked
-function jsonpcallback(response) {
-    $('input[type=submit]').removeAttr('disabled')
-    $('#response').removeClass('hidden')
-
-    var text = JSON.stringify(response, null, '&nbsp;')
-
-    $('#response').html('<strong>' + response.message + '</strong><br/>' + text)
-}
 
